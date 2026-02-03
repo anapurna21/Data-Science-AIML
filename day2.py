35ac07ca0337610f801453460a86ff9205966abb
unknown <annapurnakulkarnik@gmail.com>
2026-02-03 15:27:15 +0530

    First commit

---
+++ day2.py
@@ -0,0 +1,27 @@
+age = 21
+height = 5.11
+name = "anu"
+is_student = True
+
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
+
+
+
+print ("arthimetic operations :")
+a = 10
+b = 5
+c = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+print(a+c*b**b)
+
+
+
+
