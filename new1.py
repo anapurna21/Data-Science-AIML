2981361ff94263eb7a87639da4350a989505249c
anapurna21 <annapurnakulkarnik@gmail.com>
2026-02-09 12:41:59 +0530

    Add files via upload

---
+++ new1.py
@@ -0,0 +1,4 @@
+message = " Hello, world! "
+print(message.strip())
+print(message.upper())
+print(message.replace("World", "Python"))
---
+++ new4.py
@@ -0,0 +1,4 @@
+num = (1,2,3,4)
+print(num)
+print("First element:", num[0])
+print("Last element:",num[-1])
